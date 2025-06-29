91e41f1b8526adff417e17cd5eb59931eab553d5
KaoM3 <152694740+KaoM3@users.noreply.github.com>
2025-06-30 00:41:50 +0800

    feat(user): Created API to delete a user

---
+++ src/pages/api/user/delete.ts
@@ -0,0 +1,47 @@
+import pool from '@/lib/db';
+import { ResultSetHeader } from 'mysql2';
+import { NextApiRequest, NextApiResponse } from 'next';
+
+/**
+* API handler to remove a user
+* 
+* @param {NextApiRequest} req Incoming request containing:
+* - `id`: The ID of the `user`
+* @param {NextApiResponse} res Response object containing status message.
+*/
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method !== 'DELETE') {
+    return res.status(405).json({ message: 'Method Not Allowed' });
+  }
+  
+  const { id } = req.body;
+  
+  // Basic validation
+  const userIdNum = Number(id)
+  if (!id || isNaN(userIdNum)) {
+    return res.status(400).json({ message: 'Invalid input: id is invalid' });
+  }
+  
+  try {
+    const conn = await pool.getConnection();
+    
+    // Delete cart item from user
+    try {
+      const [result] = await conn.query<ResultSetHeader>(
+        'DELETE FROM user WHERE id = ?',
+        [userIdNum]
+      )
+      if (result.affectedRows > 0) {
+        return res.status(200).json({ message: 'Item successfully deleted' })
+      } else {
+        return res.status(404).json({ message: 'Nothing to delete' })
+      }
+    } finally {
+      conn.release();
+    }
+    
+  } catch (err) {
+    console.error('DB Error:', err);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+}
